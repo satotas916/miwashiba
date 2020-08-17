17773bc09a6cfeba19112717b7e51d830e202742
ediplex_sato <sato@ediplex.com>
2020-08-17 19:25:09 +0900

    gallary増やした

--- src/statics/js/main.js
+++ src/statics/js/main.js
@@ -1,5 +1,24 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper(".c-kv", {
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
+  },
+});
+
+var galleryThumbs = new Swiper(".p-gallery-game__thumbnail", {
+  spaceBetween: 10,
+  slidesPerView: 6,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+
+var galleryTop = new Swiper(".p-gallery-game__slide", {
+  spaceBetween: 10,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: galleryThumbs,
   },
 });
